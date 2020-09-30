63cffb122e61fc608ce47eee2594902a374d6730
snowden.xu <snowden.xu@mobilemd.cn>
2020-09-30 13:34:06 +0800

    feat: 新增路由过场动画

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, Spin } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
@@ -10,6 +10,7 @@ moment.locale('zh-cn');
 
 // 自定义
 import './global.less';
+import styles from './App.less';
 import { routerData } from '@/router';
 import Nav from '@/layouts/Nav';
 import store from '@/store/store';
@@ -20,9 +21,15 @@ const App = () => {
       <Provider store={store}>
         <Router>
           <Nav />
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense
+            fallback={
+              <div className={styles.loading}>
+                <Spin size="large" tip="页面加载中..." />
+              </div>
+            }
+          >
             <Switch>
-              {routerData.map(item => {
+              {routerData.map((item) => {
                 return <Route exact path={item.path} component={item.component} key={item.path} />;
               })}
               <Redirect to="/" />
